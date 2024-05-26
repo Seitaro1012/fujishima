23994f5d5755fe32a79ba04abb118954ae12f4af
kitahara <mizupe1223@gmail.com>
2024-05-26 20:59:44 +0900

    仮フッタープッシュ

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,115 +2,75 @@
 
 const gu_header = `
 <header class="fk-header">
-<h1 class="fk-header__logo"><a href="/" tabindex="0"><img src="/assets/images/logo.png" alt="GUSH.Inc"></a></h1>
-<ul class="fk-header__navList">
-  <li>
-    <a href="/vision/" tabindex="0">理念について</a>
-  </li>
-  <li>
-    <a href="/services/" tabindex="0">事業について</a>
-  </li>
-  <li>
-    <a href="/recruit/" tabindex="0">採用について</a>
-  </li>
-  <li>
-    <a href="https://note.com/apark_nagano" target="_blank" aria-label="お知らせを新規タブで開きます" rel="noopener noreferrer" tabindex="0">お知らせ</a>
-  </li>
-  <li>
-    <a href="/contact/" tabindex="0">問い合わせ</a>
-  </li>
-  <li>
-    <a href="https://www.thepith-store.com/" target="_blank" aria-label="オンラインストアを新規タブで開きます" rel="noopener noreferrer" tabindex="0">オンラインストア</a>
-  </li>
-</ul>
-<button class="fk-mobile-menu-icon js-toggleButton" tabindex="0">
-  <span></span>
-  <span></span>
-  <span></span>
-</button>
-</header>
-<div class="fk-overlay">
-<div class="fk-overlay__header">
-  <p class="fk-overlay__headerLogo"><a href="/" tabindex="0"><img src="/assets/images/logo_w.png" alt="GUSH.Inc"></a></p>
-  <p class="fk-overlay__headerAddress">〒386-0034  長野県上田市中之条576-1 スカイビル 105</p>
-  <ul class="fk-overlay__headerSnsIcons">
-    <li><a href="__dummy__" target="_blank" aria-label="インスタグラムを新規タブで開きます" rel="noopener noreferrer" tabindex="0"><img src="/assets/images/icon01.png" alt="インスタグラム"></a></li>
-    <li><a href="https://maps.app.goo.gl/7Z465CwU9qYgyo5w8" target="_blank" aria-label="Googleマップを新規タブで開きます" rel="noopener noreferrer"><img src="/assets/images/icon02.png" alt="Googleマップ" tabindex="0"></a></li>
+  <a class="fk-header__logo" href="/" tabindex="0">
+    <img class="fk-header__logoImage" src="/assets/images/logo01.png" alt="藤島建設のロゴ">
+    <h1 class="fk-header__logoText"><img src="/assets/images/logo_text01.svg" alt="藤島建設"></h1>
+  </a>
+
+  <ul class="fk-header__navList">
+    <li>
+      <a href="__dummy__" tabindex="0">ご挨拶</a>
+    </li>
+    <li>
+      <a href="__dummy__" tabindex="0">施工事例</a>
+    </li>
+    <li>
+      <a href="__dummy__" tabindex="0">会社概要</a>
+    </li>
+    <li>
+      <a class="fk-button-1 fk-button__iconMail" href="__dummy__" tabindex="0">お問い合わせ</a>
+    </li>
   </ul>
-</div>
-<nav class="fk-overlay__nav">
-  <ul class="fk-overlay__navList">
-  <li>
-    <a href="/vision/" tabindex="0">理念について</a>
-  </li>
-  <li>
-    <a href="/services/" tabindex="0">事業について</a>
-  </li>
-  <li>
-    <a href="/recruit/" tabindex="0">採用について</a>
-  </li>
-  <li>
-    <a href="https://note.com/apark_nagano" target="_blank" aria-label="お知らせを新規タブで開きます" rel="noopener noreferrer" tabindex="0">お知らせ</a>
-  </li>
-  <li>
-    <a href="/contact/" tabindex="0">問い合わせ</a>
-  </li>
-  <li>
-    <a href="https://www.thepith-store.com/" target="_blank" aria-label="オンラインストアを新規タブで開きます" rel="noopener noreferrer" tabindex="0">オンラインストア</a>
-  </li>
-</ul>
-</nav>
-<p class="fk-overlay__copyright">&copy; 2024 GUSH Inc. </p>
-<div class="fk-object fk-object--overlay">
-  <div class="fk-object__shape fk-object__shape--blue02"></div>
-  <div class="fk-object__shape fk-object__shape--blue03"></div>
-</div>
-</div>
+  <!-- <button class="fk-mobile-menu-icon js-toggleButton" tabindex="0">
+    <span></span>
+    <span></span>
+    <span></span>
+  </button> -->
+</header>
 `;
 
 
 
 const gu_footer = `
 <footer class="fk-footer">
-<div class="fk-footer__container">
-  <div class="fk-footer__addressArea">
-    <p class="fk-footer__logo"><a href="/" tabindex="0"><img src="/assets/images/logo_w.png" alt="GUSH.Inc"></a></p>
-    <p class="fk-footer__address">
-      〒386-0034<br class="only-pc">長野県上田市中之条576-1<br class="only-pc">スカイビル 105
-    </p>
-    <p class="fk-footer__copyright">&copy; 2024 GUSH Inc. </p>
-  </div>
-  
-  <div class="fk-footer__mapWrap">
-    <div class="fk-footer__map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12845.865615837636!2d138.21985176605048!3d36.397909457829726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601da2d744e8f48d%3A0x8cfc656aaf93da11!2sTHE%20PITH!5e0!3m2!1sja!2sjp!4v1712408568044!5m2!1sja!2sjp" width="600" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  <div class="fk-footer__container">
+    <div class="fk-footer__addressArea">
+      <a class="fk-footer__logo" href="/" tabindex="0"><img class="fk-footer__logoImage" src="/assets/images/logo01.png" alt="藤島建設のロゴ">
+        <p class="fk-footer__logoText"><img src="/assets/images/logo_text02.svg" alt="藤島建設"></p>
+      </a>
+      <p class="fk-footer__address">
+        〒194-0038 東京都町田市根岸1-5-25
+        <br class="only-pc">TEL / FAX 042-814-7770
+      </p>
+      
     </div>
+
+    
+    <div class="fk-footer__nav">
+      <ul class="fk-footer__navSns">
+        <li><a href="__dummy__" tabindex="0"><img src="/assets/images/icon_phone_w.png" alt="__dummy__"></a></li>
+        <li><a href="__dummy__" tabindex="0"><img src="/assets/images/icon_mail_w.png" alt="__dummy__"></a></li>
+        <li><a href="__dummy__" tabindex="0"><img src="/assets/images/icon_map_w.png" alt="__dummy__"></a></li>
+      </ul>
+      <ul class="fk-footer__navList">
+        <li>
+          <a href="__dummy__" tabindex="0">ご挨拶</a>
+        </li>
+        <li>
+          <a href="__dummy__" tabindex="0">施行事例</a>
+        </li>
+        <li>
+          <a href="__dummy__" tabindex="0">会社概要</a>
+        </li>
+        <li>
+          <a href="__dummy__" tabindex="0">お問い合わせ</a>
+        </li>
+        
+      </ul>
+      <p class="fk-footer__copyright">&copy; 2020 Fujishima Kensetsu Ltd.</p>
+    </div>
+    
   </div>
-  
-  <div class="fk-footer__nav">
-    <ul class="fk-footer__navList">
-      <li>
-        <a href="/vision/" tabindex="0">理念について</a>
-      </li>
-      <li>
-        <a href="/services/" tabindex="0">事業について</a>
-      </li>
-      <li>
-        <a href="/recruit/" tabindex="0">採用について</a>
-      </li>
-      <li>
-        <a href="https://note.com/apark_nagano" target="_blank" aria-label="お知らせを新規タブで開きます" rel="noopener noreferrer" tabindex="0">お知らせ</a>
-      </li>
-      <li>
-        <a href="/contact/" tabindex="0">お問い合わせ</a>
-      </li>
-      <li>
-        <a href="https://www.thepith-store.com/" target="_blank" aria-label="オンラインストアを新規タブで開きます" rel="noopener noreferrer" tabindex="0">オンラインストア</a>
-      </li>
-    </ul>
-  </div>
-  
-</div>
 </footer>
 `;
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,16 @@ import { gu_header, gu_footer } from './layout.js';
 
 document.addEventListener('DOMContentLoaded',function(){
 
-  // document.getElementById('fk-header-js').insertAdjacentHTML('afterbegin', gu_header);
-  // document.getElementById('fk-footer-js').insertAdjacentHTML('afterbegin', gu_footer);
+  document.getElementById('fk-header-js').insertAdjacentHTML('afterbegin', gu_header);
+  document.getElementById('fk-footer-js').insertAdjacentHTML('afterbegin', gu_footer);
 
-  // document.querySelector('.js-toggleButton').addEventListener('click', function(){
-  //   document.querySelector('body').classList.toggle('js-overlay');
-  //   const ol = document.querySelector('.fk-overlay');
-  //   ol.classList.toggle('js-show');
-  //   document.querySelector('.fk-mobile-menu-icon').classList.toggle('js-open');
+  document.querySelector('.js-toggleButton').addEventListener('click', function(){
+    document.querySelector('body').classList.toggle('js-overlay');
+    const ol = document.querySelector('.fk-overlay');
+    ol.classList.toggle('js-show');
+    document.querySelector('.fk-mobile-menu-icon').classList.toggle('js-open');
 
-  // });
+  });
 
 });
 
